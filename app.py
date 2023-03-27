5a371559319dca8d830dedb372cfb815ffcc1ae7
Steb <72015326+EstebanLouerat@users.noreply.github.com>
2023-03-27 12:10:58 +0200

    Add templates

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask
+from flask import request
 from markupsafe import escape
+import sqlite3
 
 app = Flask(__name__)
 
+# Connection to the DB
+conn = sqlite3.connect('database.db')
+
+# Create the table
+with open('schema.sql', 'r') as f:
+    schema = f.read()
+conn.executescript(schema)
+
+# Close the BD connection
+conn.close()
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+
 @app.route("/<name>")
 def hello(name):
-    return f"Hello, {escape(name)}!"
+    return f"<h2>Hello, {escape(name)}!<h2>"
 
 @app.route('/')
 def index():
